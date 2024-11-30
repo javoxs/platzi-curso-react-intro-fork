5ed999102d1b08c1b5df8f032a3ef6d1229b21c7
jjcc <jjcc2891@gmail.com>
2024-11-30 12:12:10 -0300

    practica useEffect

--- src/App/index.js
+++ src/App/index.js
@@ -2,16 +2,15 @@ import React from 'react';
 import { AppUI } from './AppUI';
 import { useLocalStorage } from './useLocalStorage';
 
-// const defaultTodos = [
-//   { text: 'Cortar cebolla', completed: true },
-//   { text: 'Tomar el Curso de Intro a React.js', completed: false },
-//   { text: 'Llorar con la Llorona', completed: false },
-//   { text: 'LALALALALA', completed: false },
-//   { text: 'Usar estados derivados', completed: true },
-// ];
+const defaultTodos = [
+  { text: 'Cortar cebolla', completed: true },
+  { text: 'Tomar el Curso de Intro a React.js', completed: false },
+  { text: 'Llorar con la Llorona', completed: false },
+  { text: 'LALALALALA', completed: false },
+  { text: 'Usar estados derivados', completed: true },
+];
 
-// localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
-// localStorage.removeItem('TODOS_V1');
+localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
 
 function App() {
   const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
@@ -31,10 +30,17 @@ function App() {
   // React.useEffect(() => {
   //   console.log('Loooooooog 2');
   // }, []);
-
+  
+  // React useEffect hook with a dependency array
+  // Dependencies are the second argument of the useEffect hook
+  // It can be an empty array, an array with values or variables
+  // If the dependency array is empty, the useEffect hook will only run once
+  // If the dependency array has variables, the useEffect hook will run every time the variables change
+  // For example, if the dependency array has a variable called totalTodos, the useEffect hook will run every time the totalTodos variable changes
+  
   React.useEffect(() => {
     console.log('Loooooooog 2');
-  }, [totalTodos]);
+  }, []);
 
   console.log('Log 3');
 
